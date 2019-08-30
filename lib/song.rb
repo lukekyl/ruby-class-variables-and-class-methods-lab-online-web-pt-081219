13ccc5cd287265a005c0c7db734c655cea17621d
Kyle Luke <th.oldschool@gmail.com>
2019-08-30 17:42:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,6 +36,14 @@ class Song
     @@genres.uniq
   end
   def self.genre_count
-    @@genres.count
+    genre_hash = {}
+    @@genres.each {|genre|
+      if !genre_hash.include? genre
+        genre_hash[genre] = 1 
+      else
+        genre_hash[genre] += 1
+      end
+    }
+    genre_hash
   end
 end
