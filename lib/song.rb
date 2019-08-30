2386c4ebbddee93064f2795b5b805ec8b1ebe84e
Kyle Luke <th.oldschool@gmail.com>
2019-08-30 17:25:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,29 @@
 class Song 
-  attr_accessor :name, :artist, :genre 
-  
+  attr_accessor :name
+  attr_reader :artist, :genre
+  @@artist_array = []
+  @@genre_array = []
   @@number_of_songs = 0
+  @@number_of_artists = 0 
+  @@number_of_genres = 0
   
-  def initialize
+  def initialize(artist, genre)
     @@number_of_songs += 1
+    @artist = artist
+    if !@artist.include? artist
+      @@artist_array << artist
+    end
+      
   end
   
   def self.count
     @@number_of_songs
   end
   
-  
+  def self.artists 
+    @@artist_array
+  end
+  def self.artist_count
+    @@artist_array.count
+  end
 end
