e764bf4500b180dcbac873af95518f743f21c228
Kyle Luke <th.oldschool@gmail.com>
2019-08-30 17:28:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   @@genre_array = []
   @@number_of_songs = 0
   
-  def new(name, artist, genre)
+  def initialize(name, artist, genre)
     @@number_of_songs += 1
     @name = name
     @artist = artist
